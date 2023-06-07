d8e4657c7afecb7a5e019afbb47e9f583e45792d
Sergey Scoville <alexscoville@outlook.com>
2023-06-07 15:24:51 +0200

    rearranging and cleaning up directories

--- Offline_GEP_Testing_Python_Scripts/combining_and_rescaling.py
+++ Offline_GEP_Testing_Python_Scripts/combining_and_rescaling.py
@@ -6,23 +6,42 @@ import ROOT
 import sys
 from PIL import Image
 
-try:
-    bins = int(sys.argv[4])
-except IndexError:
-    bins = 0
+if "--multifile" in sys.argv:
+    try:
+        bins = int(sys.argv[4])
+    except IndexError:
+        bins = 0
+else:
+    try:
+        bins = int(sys.argv[2])
+    except IndexError:
+        bins = 0
 
-# Open the ROOT files
-file1 = ROOT.TFile(sys.argv[1])
-file2 = ROOT.TFile(sys.argv[2])
-file3 = ROOT.TFile(sys.argv[3])
+if "--multifile" in sys.argv:
+    file1 = ROOT.TFile(sys.argv[1]) 
+    file2 = ROOT.TFile(sys.argv[2])
+    file3 = ROOT.TFile(sys.argv[3])
 
-# Get the histogram names from each file
-histogram1_name = file1.GetListOfKeys()[0].GetName()
-histogram2_name = file2.GetListOfKeys()[0].GetName()
-histogram3_name = file3.GetListOfKeys()[0].GetName()
+    # Get the histogram names from each file
+    histogram1_name = file1.GetListOfKeys()[0].GetName()
+    histogram2_name = file2.GetListOfKeys()[0].GetName()
+    histogram3_name = file3.GetListOfKeys()[0].GetName()
 
-hists = [histogram1_name, histogram2_name, histogram3_name]
-hist_legend_names = ["", "", ""]
+    hists = [histogram1_name, histogram2_name, histogram3_name]
+
+    # Get the histograms from each file
+    hist1 = file1.Get(histogram1_name)
+    hist2 = file2.Get(histogram2_name)
+    hist3 = file3.Get(histogram3_name)
+else:
+    file = ROOT.TFile(sys.argv[1])
+
+    hist1 = file.Get("h_Calo422TopoClusters_N")
+    hist2 = file.Get("h_Calo420TopoClusters_N")
+    hist3 = file.Get("h_CaloCalTopoClusters_N")
+    hist_legend_names = ["", "", ""]
+
+    hists = ["h_Calo422TopoClusters_N", "h_Calo420TopoClusters_N", "h_CaloCalTopoClusters_N"]
 
 for i in range(len(hists)):
     if "Calo422" in hists[i]:
@@ -31,11 +50,6 @@ for i in range(len(hists)):
         hist_legend_names[i] = "Calo_420"
     elif "CaloCal" in hists[i]:
         hist_legend_names[i] = "Calo_Cal"
-
-# Get the histograms from each file
-hist1 = file1.Get(histogram1_name)
-hist2 = file2.Get(histogram2_name)
-hist3 = file3.Get(histogram3_name)
 
 hist1.SetName(hist_legend_names[0])
 hist2.SetName(hist_legend_names[1])
@@ -69,7 +83,8 @@ def get_last_bin(histogram):
 def histogram_modifiers(individual_histograms, xmax, ymax, bins):
     for i in individual_histograms:
         i.GetXaxis().SetRangeUser(0, xmax+50)
-        i.GetYaxis().SetRangeUser(0, ymax+0.01)
+        i.GetYaxis().SetRangeUser(0, 1.1*ymax)
+        i.SetLineWidth(2)
     return
 
 
@@ -144,6 +159,8 @@ hist1.GetYaxis().SetTitle("Fraction of events")
 hist1.GetYaxis().CenterTitle()
 hist1.GetYaxis().SetTitleOffset(1.6)
 hist1.GetYaxis().SetNdivisions(-6, ROOT.kFALSE) # Removing minor tickmarks
+hist1.SetFillColorAlpha(ROOT.kBlue, 0.1)
+hist1.SetFillStyle(3144)
 
 
 hist1.Draw()
@@ -159,6 +176,8 @@ stats_box1.SetY2NDC(0.95)  # Set Y-coordinate of the upper-right corner
 
 hist2.SetLineColor(ROOT.kRed)
 hist2.Draw("SAMES")
+hist2.SetFillColorAlpha(ROOT.kRed, 0.1)
+hist2.SetFillStyle(3490)
 
 canvas.Update()
 
@@ -170,8 +189,9 @@ stats_box2.SetX2NDC(0.95)  # Set X-coordinate of the upper-right corner
 stats_box2.SetY2NDC(0.8)
 
 
-hist3.SetLineColor(ROOT.kGreen)
+hist3.SetLineColor(ROOT.kGreen+2)
 hist3.Draw("SAMES")
+hist3.SetFillColorAlpha(ROOT.kGreen+2, 0.1)
 
 canvas.Update()
 
@@ -197,14 +217,18 @@ legend.Draw("same")
 canvas.Update()
 ROOT.gEnv.SetValue("Hist.PictureCompress", 1)
 
-canvas.SaveAs("combining_and_rescaling_TESTING.png")
-canvas.SaveSource("combining_and_rescaling_10_rebin_TESTING.C")
+canvas.SaveAs("hist_Et0_"+str(bins)+"_rebin.png")
+canvas.SaveSource("hist_Et0_"+str(bins)+"_rebin.C")
 
 output_file = ROOT.TFile("combined_histograms_"+str(bins)+"_rebin.root", "RECREATE")
 canvas.Write()
 
 output_file.Close()
 
-file1.Close()
-file2.Close()
-file3.Close()
+if "--multifile" in sys.argv:
+    file1.Close()
+    file2.Close()
+    file3.Close()
+
+else:
+    file.Close()
